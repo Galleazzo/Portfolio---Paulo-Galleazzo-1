68b121169ac7b348e8bb4376dea0bfb09b9979eb
Galleazzo <paulogalleazzo1@gmail.com>
2023-01-24 21:28:43 -0300

    updates

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+function liteMod(){
+
+    document.main.style.backgroundColor = "#ffffff"
+    document.main.style.color = "#0b131d"
+}
+
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".head").classList.toggle("show-menu")
 );
